a460d65b49e713f293bb395effcd12937686b79e
Moaz Ashraf <mouadhashraf@gmail.com>
2018-08-15 22:41:37 +0200

    Update asm.c indentation style

--- asm.c
+++ asm.c
@@ -2,102 +2,102 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXREAD 8192	// buffer size for reading from stdin
+#define MAXREAD 8192    // buffer size for reading from stdin
 
-char *prog;		// program name
+char *prog; // program name
 
 void printerr(char *, ...);
 
 // assembles Intel 8080 source code.
 int main(int argc, char *argv[])
 {
-	prog = argv[0];				// get the command line name
-	char *srcbuf;				// source code buffer
-	char *srcp;					// pointer in srcbuf
-	long unsigned srcsize;		// source code buffer size
 
-	if (argc == 1) {
-		char readbuf[MAXREAD];	// buffer for reading
-		char *rbufp;			// pointer in readbuf
-		char c;
+    prog = argv[0];             // get the command line name
+    char *srcbuf;               // source code buffer
+    char *srcp;                 // pointer in srcbuf
+    long unsigned srcsize;      // source code buffer size
 
-		// read into readbuf
-		for (rbufp = readbuf; rbufp-readbuf < MAXREAD
-		 && (c = getchar()) != EOF;)
-			*rbufp++ = c;
+    if (argc == 1) {
+        char readbuf[MAXREAD];  // buffer for reading
+        char *rbufp;            // pointer in readbuf
+        char c;
 
-		// copy to srcbuf
-		srcsize = rbufp-readbuf;
-		if ((srcbuf = (char *) malloc(srcsize+1)) == NULL) {	// +1 for terminating '\0'
-			printerr("error: cannot allocate memory");
-			return 1;
-		}
-		strncpy(srcbuf, readbuf, srcsize);
-	} else {
-		FILE *fp;													// file pointer
-		char **fbufs;												// file buffers
-		int fcnt = argc-1;											// file count
+        // read into readbuf
+        for (rbufp = readbuf; rbufp-readbuf < MAXREAD
+         && (c = getchar()) != EOF;)
+            *rbufp++ = c;
 
-		if ((fbufs = (char **) malloc(fcnt * sizeof(char**))) == NULL) {
-			printerr("error: cannot allocate memory");
-			return 1;
-		}
-		char **fbp = fbufs;											// pointer in fbufs
-		srcsize = 0;
+        // copy to srcbuf
+        srcsize = rbufp-readbuf;
+        if ((srcbuf = (char *) malloc(srcsize+1)) == NULL) {    // +1 for terminating '\0'
+            printerr("error: cannot allocate memory");
+            return 1;
+        }
+        strncpy(srcbuf, readbuf, srcsize);
+    } else {
+        int fcnt = argc-1;  // file count
+        FILE *fp;           // file pointer
+        char **fbufs;       // file buffers
 
-		while (--argc > 0) {
-			// open file stream
-			if ((fp = fopen(*++argv, "rb")) == NULL) {
-				printerr("error: cannot open file %s", *argv);
-				return 1;
-			}
-			
-			// get size of file
-			fseek(fp, 0L, SEEK_END);
-			long unsigned fsize = ftell(fp);
-			srcsize += fsize;
-			fseek(fp, 0L, SEEK_SET);
+        if ((fbufs = (char **) malloc(fcnt * sizeof(char**))) == NULL) {
+            printerr("error: cannot allocate memory");
+            return 1;
+        }
+        char **fbp = fbufs; // pointer in fbufs
+        srcsize = 0;
 
-			// read file in a buffer
-			if ((*fbp = (char *) malloc(fsize+1)) == NULL) {
-				printerr("error: cannot allocate memory");
-				return 1;
-			}
-			if (fread(*fbp, 1, fsize, fp) != fsize) {
-				printerr("error: cannot read file %s", *argv);
-				return 1;
-			}
-			(*fbp)[fsize] = '\0';
-			fbp++;
-			fclose(fp);		// close file stream
-		}
+        while (--argc > 0) {
+            // open file stream
+            if ((fp = fopen(*++argv, "rb")) == NULL) {
+                printerr("error: cannot open file %s", *argv);
+                return 1;
+            }
+            
+            // get size of file
+            fseek(fp, 0L, SEEK_END);
+            long unsigned fsize = ftell(fp);
+            srcsize += fsize;
+            fseek(fp, 0L, SEEK_SET);
 
-		// concatenate all files into srcbuf
-		if ((srcbuf = (char *) malloc(srcsize+1)) == NULL) {
-			printerr("error: cannot allocate memory");
-			return 1;
-		}
-		srcp = srcbuf;
-		fbp = fbufs;
-		argc = fcnt+1;
+            // read file in a buffer
+            if ((*fbp = (char *) malloc(fsize+1)) == NULL) {
+                printerr("error: cannot allocate memory");
+                return 1;
+            }
+            if (fread(*fbp, 1, fsize, fp) != fsize) {
+                printerr("error: cannot read file %s", *argv);
+                return 1;
+            }
+            (*fbp)[fsize] = '\0';
+            fbp++;
+            fclose(fp); // close file stream
+        }
 
-		while (--argc > 0) {
-			strcpy(srcp, *fbp);
-			srcp += strlen(*fbp);
-			free(*fbp);
-			fbp++;
-		}
+        // concatenate all files into srcbuf
+        if ((srcbuf = (char *) malloc(srcsize+1)) == NULL) {
+            printerr("error: cannot allocate memory");
+            return 1;
+        }
+        srcp = srcbuf;
+        fbp = fbufs;
+        argc = fcnt+1;
 
-		free(fbufs);
-	}
-	srcbuf[srcsize] = '\0';
-	// TODO: collect labels, instructions and pseudo instructions
-	free(srcbuf);
-	// TODO: start assembly
-	// TODO: output machine code
-	// TODO: free any other buffers
+        while (--argc > 0) {
+            strcpy(srcp, *fbp);
+            srcp += strlen(*fbp);
+            free(*fbp);
+            fbp++;
+        }
 
-	return 0;
+        free(fbufs);
+    }
+    srcbuf[srcsize] = '\0';
+    // TODO: collect labels, instructions and pseudo instructions
+    free(srcbuf);
+    // TODO: start assembly
+    // TODO: output machine code
+    // TODO: free any other buffers
+    return 0;
 }
 
 #include <stdarg.h>
@@ -105,11 +105,11 @@ int main(int argc, char *argv[])
 // printerr: prints a formatted error message.
 void printerr(char *fmt, ...)
 {
-	va_list args;
-	
-	va_start(args, fmt);
-	fprintf(stderr, "%s: ", prog);
-	vfprintf(stderr, fmt, args);
-	fprintf(stderr, "\n");
-	va_end(args);
+    va_list args;
+    
+    va_start(args, fmt);
+    fprintf(stderr, "%s: ", prog);
+    vfprintf(stderr, fmt, args);
+    fprintf(stderr, "\n");
+    va_end(args);
 }
